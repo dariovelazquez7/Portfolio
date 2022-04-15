4c01c9a40766348d0a5f8440ea94c390fb69bdc3
dariovelazquez7 <dario.velaquez10@gmail.com>
2022-04-15 17:38:14 -0300

    fix: el formulario no envia el correo, se cambia gmal por outlook

--- back/main.ts
+++ back/main.ts
@@ -4,12 +4,15 @@ require('dotenv').config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
+const colors = require('colors');
+const smtpTransport = require("nodemailer-smtp-transport");
+
 const {EMAIL_TRANSPORTER, PASS, MY_EMAIL, PORT} = process.env
 
 
 const app:Application = express()
 app.listen(PORT || 3001, () => {
-    console.log(`servidor corriendo en puerto ${PORT}!`)
+    console.log(colors.blue(`servidor corriendo en el puerto ${PORT}!`))
 })
 
 app.use(express.urlencoded({extended: false}))
@@ -25,33 +28,48 @@ app.use(cors(corsOptions))
 
 app.post("/sendEmail",  async(req:Request,res:Response) => {
     const {name, email, msj} = req.body
+    let nameCapitalized = name.split(" ").map((e:string) => e.charAt(0).toUpperCase() + e.slice(1)).join(" ")
     const contHTML= 
-        `<h3> Información de contacto:</h3>
-            <ul>
-                <li>Nombre: ${name} </li>
-                <li>Email: ${email} </li>
-            </ul>
-        <p>Mensaje: ${msj}</p>
-        `
+    `<div style="font-size:15px; width: 40%; background: #f7f7f7; padding:18px;text-align: center; border-radius: 5px;">
+        <h2 >¡Hola Darío!</h2>
+        <div style="width: 59%;margin: auto">
+            <p ><b> ${nameCapitalized}</b> te ha enviado un correo desde tu portfolio:</p>
+                <div style="margin: auto; text-align:center;background:#ededed;min-height: 75px;
+                    vertical-align: middle;line-height: 32px;padding: 5px; border-radius: 5px;">
+                    <b>"${msj}"</b>
+                </div>
+        </div>   
+        <p>Para contactarte con esta persona, envia tu respuesta al siguiente email: <br> 
+        ${email}
+        </p> 
+    </div>`
     try{
-        const transporter= nodemailer.createTransport({
-        // host: "smtp.gmail.com",
-        // port: 465,
-        // secure: true,
-        service: 'gmail',
+        const transporter= nodemailer.createTransport(smtpTransport({
+        host: "smtp-mail.outlook.com",
+        port: 587,
+        secure: false,  
         auth:{
             user: `${EMAIL_TRANSPORTER}`,  
             pass: `${PASS}`
-        }
+        },
         })
-        await transporter.verify()
-        await transporter.sendMail({
+        )
+         transporter.sendMail({
             from: `'Portfolio' <${EMAIL_TRANSPORTER}>`,
             to: `${MY_EMAIL}`,
             subject: "[Nuevo email desde MyPortfolio]",
             html: contHTML
-        })
-        return res.send("email enviado correctamente")
+        }, function (error:any, info:any) {
+            if (error) {
+              console.log(error);
+              res.send("No se pudo enviar el email")
+            } else {
+              console.log("Email sent: " + info.response);
+              return res.send("email enviado correctamente")
+            }
+          }
+        
+        )
     }
     catch(err){
         return res.send("nono")
